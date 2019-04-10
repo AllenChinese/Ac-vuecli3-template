b6d88f2375fa34c95e4de68e67502e0142b23eb7
AllenChinese <13402202685@163.com>
2019-04-11 01:04:02 +0800

    chore: ElementUI 按需加载配置

---
+++ src/elementConfig.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+// ElementUI 按需加载
+import {
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Badge
+} from 'element-ui';
+
+Vue.use(Menu);
+Vue.use(Submenu);
+Vue.use(MenuItem);
+Vue.use(MenuItemGroup);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Badge);
